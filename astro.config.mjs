bba8ff565e4c0bd4ffcabb8173c99f631fc14029
sman247 <115250473+sman247@users.noreply.github.com>
2024-06-03 23:19:54 +0100

    website name in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // // https://astro.build/config
 // export default defineConfig({
-//     site: "https://seanmansfield.com",
+//     site: "https://maddyweavers.com",
 //     base: "/",
 // });
